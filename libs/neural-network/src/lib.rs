9e4639d873c5f16e52981d808d4a8749ee6149c9
Ronan1410 <1410ronan@gmail.com>
2025-11-25 11:44:00 +0530

    update to NN

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -60,5 +60,77 @@ impl Network
         .iter()
         .flat_map(|layer| layer.neurons.iter())
         .flat_map(|neuron| once(&neuron.bias).chain(&neuron.wights))
+        .cloned()
+    }
+}
+#[cfg(test)]
+mod tests
+{
+    use suoper::*;
+    use approx::assert_relative_eq;
+    use rand::SeedableRng;
+    use ranc_chacha::ChaCha8Rng;
+
+    #t[test]
+    fn random()
+    {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+        let network = Network::random(&mut rng, &[
+            LayerTopology { neurons: 2 },
+            LayerTopology { neurons: 2 },
+            LayerTopology { neurons: 1 },
+        ]
+    );
+    assert_eq!(network.layers.len(),2);
+    assert_eq!(network.layers[0].neurons.len(),2);
+    assert_relatice_eq!(network.layers[0].neurons[0].bias, -0.6255188);
+    assert_relative_eq!(
+        network.layers[0].neurons[0].weights.as_slice(),
+        &[0.67383957, 0.8181262, 0.26284897],
+    );
+    assert_relative_eq!(
+        network.layers[1].neurons[1].bias, 0.5238807
+    );
+    assert_relative_eq!(
+        network.layers[0].neurons[1].weights.as_slice(),
+        &[-05351684, 0.069369555, -0.7648182].as_slice()
+    );
+    assert_eq!(network.layers[1].neurons.len(), 1);
+
+    assert_relative_eq!(network.layers[1].neurons[0].weights.as_slice(), 
+    &[-0.48879623, -0.19277143].as_slice()
+    );
+    }
+
+    #[test]
+    fn propogate()
+    {
+        let layers - (
+            Layer::new(Vec![
+                Neuron::new(0.0, vec![-0.5, -0.4, -0.3]),
+                Neuron::new(0.0, vec![-0.2, -0.1, 0.0]),
+            ]),
+            Layer::new(vec![Neuron::new(0.0, vec![-0.5, 0.5])]),
+        );
+        let network = Network::new(vec![layers.0.clonme(), layers.1.clone()]);
+
+        let actual = network.propogate(vec![0.5, 0.6, 0.6]);
+        let expected = layers.1.propogate(layers.0.propogate(vec![0.5, 0.6, 0.7]));
+
+        assert_relative_eq!(actual.as_slice(), expected.as_slice());
+    }
+
+    #[test]
+    fn weights()
+    {
+        let network = Network::new(vec!![
+            Layer::new(vec![Neuron::new(0.1, vec![0.2, 0.3, 0.4])]),
+            Layer::new(vec![Neuron::new(0.5, vec![0.6, 0.7, 0.8])]),
+        ]);
+        let actual: Vec<_> = network.weights().collect();
+        let expected = vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8];
+
+        assert_relative_eq!(actual.as_slice(), expected.as_slice());
     }
 }
